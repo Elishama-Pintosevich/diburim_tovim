18d7f0c34f8d5f8ca1976769521bdf04998abfb7
Elishama-Pintosevich <pintosevich2000@gmail.com>
2024-05-10 12:20:33 +0300

    test

--- routes/ivr.py
+++ routes/ivr.py
@@ -18,9 +18,10 @@ class Ivr(MethodView):
         resp = VoiceResponse()
 
         # Read a message aloud to the caller
-        resp.say("Thank you for calling! Have a great day.", voice='Polly.Amy')
+        # resp.say("Thank you for calling! Have a great day.", voice='Polly.Amy')
 
-        return str(resp)
+        # return str(resp)
+        return "hi"
         
 
 
