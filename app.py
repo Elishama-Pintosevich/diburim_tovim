7a228e17e8ba8474557c62a9d4eb6201887b6be5
Elishama-Pintosevich <pintosevich2000@gmail.com>
2024-05-10 15:15:07 +0300

    twilio start

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask , request
 from flask_smorest import Api
 from routes.ivr import blp as IvrBlueprint
 from flask_cors import CORS
+import os
+from twilio.rest import Client
 
 def create_app():
     app = Flask(__name__)
 
     CORS(app)
+
+    account_sid = os.environ['ACCOUNT_SID']
+    auth_token = os.environ['TOKEN_SID']
+    client = Client(account_sid, auth_token)
+
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Diburim tovim - IVR, REST API"
     app.config["API_VERSION"] = "v1"
