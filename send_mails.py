028ff42fe40e59101e90364010c876a64d27e4f3
Elishama-Pintosevich <pintosevich2000@gmail.com>
2024-05-17 17:56:20 +0300

    fix html2

--- send_mails.py
+++ send_mails.py
@@ -33,19 +33,19 @@ def send_mail(mail_data):
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
-    <body style="margin: 0;">
-    <div dir="rtl" style="background-color: rgb(191 219 254); color: black; padding: 30px 30px 800px 30px;">
-        <div style="box-shadow: 1px 1px px black; border-radius: 20px;">
-            <div style=" padding: 18px; background-color: rgb(30 64 175); color: black; border-radius: 20px 20px 0px 0px;">
-                <div style="font-family: sans-serif; font-size: 24px; font-weight: bold; color: white; text-decoration: none;">דיבורים טובים</div>
-            </div>
-            <div style=" background-color: white; padding: 30px 30px 40px 30px; border-radius: 0px 0px 20px 20px;">
-                <div style="font-size: 20px; padding: 0px 0px 10px 0px; font-family: sans-serif;">{subject}</div>
-                <a style=" color: black; text-decoration: none;font-family: sans-serif;" href="tel:{message}">מספר טלפון:{message} </a>
+    <body>
+        <div dir="rtl" style="background-color: rgb(191 219 254); color: black; padding: 30px 30px 800px 30px;">
+            <div style="box-shadow: 1px 1px px black; border-radius: 20px;">
+                <div style=" padding: 18px; background-color: rgb(30 64 175); color: black; border-radius: 20px 20px 0px 0px;">
+                    <div style="font-family: sans-serif; font-size: 24px; font-weight: bold; color: white; text-decoration: none;">דיבורים טובים</div>
+                </div>
+                <div style=" background-color: white; padding: 30px 30px 40px 30px; border-radius: 0px 0px 20px 20px;">
+                    <div style="font-size: 20px; padding: 0px 0px 10px 0px; font-family: sans-serif;">{subject}</div>
+                    <a style=" color: black; text-decoration: none;font-family: sans-serif;" href="tel:{message}">מספר טלפון:{message} </a>
+                </div>
             </div>
         </div>
-    </div>
-</body> 
+    </body> 
     </html>
     """
     part1 = MIMEText(text, 'plain')
